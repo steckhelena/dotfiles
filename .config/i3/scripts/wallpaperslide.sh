aa250115a4e07ec8f4d0ecf1a2baf8ac77c73e48
Helena Steck <steckhelena@gmail.com>
2017-03-25 11:41:28 -0300

    Added a wallpaper manager script.

---
+++ .config/i3/scripts/wallpaperslide.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+bgfolder="$HOME/.wallpaper"
+bgtimer=300
+
+# Changes to a random wallpaper in bgfolder after the seconds defined by bgtimer
+while true
+do
+feh --randomize --bg-fill "$bgfolder/*"
+sleep bgtimer
+done
