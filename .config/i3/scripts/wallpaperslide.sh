45cdf2a5926314e5450d7a4d41e14f6d105955b6
Helena Steck <steckhelena@gmail.com>
2017-03-25 12:04:07 -0300

    Fixed wallpaper manager scrpit.

--- .config/i3/scripts/wallpaperslide.sh
+++ .config/i3/scripts/wallpaperslide.sh
@@ -6,6 +6,6 @@ bgtimer=300
 # Changes to a random wallpaper in bgfolder after the seconds defined by bgtimer
 while true
 do
-feh --randomize --bg-fill "$bgfolder/*"
-sleep bgtimer
+feh --randomize --bg-fill $bgfolder/*
+sleep $bgtimer
 done
