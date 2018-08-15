0b2b352b5d8a4bc6c1396982239f3e20d2f0342e
Helena Steck <steckhelena@gmail.com>
2018-08-14 21:47:24 -0300

    Added more functionalities and made it beautiful

--- .config/i3/scripts/sysmenu.sh
+++ .config/i3/scripts/sysmenu.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-res=$(echo "Logout;Suspend;Restart;Shutdown;Cancel" | rofi -sep ";" -dmenu -p "Power:" -hide-scrollbar -width 200 -lines 5 -i)
+res=$(echo "	Logout;	Suspend;	Restart;	Shutdown;	Cancel" | rofi -sep ";" -dmenu -hide-scrollbar -i -theme PowerOffMenu)
 
-if [ $res = "Logout" ]; then
+if [[ $res = *"Logout"* ]]; then
     ~/.config/i3/scripts/graceful_shutdown.sh logout &
 fi
-if [ $res = "Suspend" ]; then
+if [[ $res = *"Suspend"* ]]; then
     ~/.config/i3/scripts/graceful_shutdown.sh suspend &
 fi
-if [ $res = "Restart" ]; then
+if [[ $res = *"Restart"* ]]; then
     ~/.config/i3/scripts/graceful_shutdown.sh restart &
 fi
-if [ $res = "Shutdown" ]; then
+if [[ $res = *"Shutdown"* ]]; then
     ~/.config/i3/scripts/graceful_shutdown.sh shutdown &
 fi
 exit 0
