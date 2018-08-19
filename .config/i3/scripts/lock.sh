0e66e082fa3bec1a6f6ec67acc8eb40dee25175a
Helena Steck <steckhelena@gmail.com>
2018-08-19 15:32:38 -0300

    Reworked lock screen and started using betterlockscreen

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-icon="$HOME/.xlock/icon.png"
-tmpbg='/tmp/screen.png'
+#icon="$HOME/.xlock/icon.png"
+tmpimg="/tmp/screen.png"
+tmpbg="$HOME/.cache/i3lock/l_blur.png"
 
-(( $# )) && { icon=$1; }
+#(( $# )) && { icon=$1; }
 
-scrot "$tmpbg"
-convert "$tmpbg" -blur 0x6 "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-i3lock -i "$tmpbg"
+scrot -z $tmpbg
+convert $tmpbg  -filter Gaussian -resize 20% \
+      -define filter:sigma=0.5 -resize 500% $tmpbg
+betterlockscreen -l blur
+#convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+#i3lock -i "$tmpbg"
