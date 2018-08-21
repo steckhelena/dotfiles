2bdf15ed8c064afec81c8643453baac4fc8d0432
Helena Steck <steckhelena@gmail.com>
2018-08-21 20:56:35 -0300

    Changed blur and dim constants

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -9,8 +9,8 @@ tmpln="$HOME/.cache/i3lock/l_blur.png"
 scrot -z $tmpimg
 rm $tmpln
 ln -s $tmpimg $tmpln
-convert $tmpln  -filter Gaussian -resize 20% -fill black -colorize 60% \
-      -define filter:sigma=0.5 -resize 500% $tmpln
+convert $tmpln  -filter Gaussian -resize 20% -fill black -colorize 50% \
+      -define filter:sigma=0.6 -resize 500% $tmpln
 betterlockscreen -l blur
 #convert "$tmpln" "$icon" -gravity center -composite -matte "$tmpbg"
 #i3lock -i "$tmpln"
