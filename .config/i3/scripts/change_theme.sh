8004e0217bdadafb67434d80f538f4c55817fef0
Helena Steck <steckhelena@gmail.com>
2021-11-07 16:58:37 -0300

    Add option to install themes when not found

--- .config/i3/scripts/change_theme.sh
+++ .config/i3/scripts/change_theme.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
 current_filename=~/.current_pybase16_theme
+themes_dir=~/.config/pybase16
 
 # recover currently selected theme
 current=$(cat $current_filename 2>/dev/null || echo '(not initialized)');
 
 # list themes
-themes=$(find ~/.config/pybase16/schemes/ -regextype posix-extended -regex '.*\.yaml|.*\.yml' -type f  \
+themes=$(find $themes_dir/schemes/ -regextype posix-extended -regex '.*\.yaml|.*\.yml' -type f  \
         | xargs -n1 basename \
         | sed 's/\.yaml$\|\.yml$//' \
         | sort \
         | sed -z 's/\n/|/g;s/|$/\n/')
+
+if [[ -z "$themes" ]]; then
+    echo $themes
+    res=$(echo 'Yes|No' | \
+        rofi -no-fixed-num-lines -sep '|' -dmenu -i \
+          -p "Do you wish to install the themes?" \
+          -theme YesNo)
+    pip install pybase16-builder
+    mkdir -p $themes_dir
+    (cd $themes_dir; pybase16 update)
+    res=$(echo 'Ok' | \
+        rofi -sep '|' -dmenu -i \
+          -p "Done!" -l 1\
+          -theme YesNo)
+    exit 0
+fi
 
 res=$(echo $themes | \
       rofi -no-fixed-num-lines -sep '|' -dmenu -i \
@@ -18,7 +35,7 @@ res=$(echo $themes | \
       -theme ThemeMenu)
 
 if [[ "$res" ]]; then
-    (cd ~/.config/pybase16;
+    (cd $themes_dir;
     pybase16 inject -s $res -f ../kitty/theme.conf -f ../nvim/theme.vim -f ../polybar/config -f ../rofi/config.rasi -f ../i3/config -f ~/.themes/FlatColor/colors2 -f ~/.themes/FlatColor/colors3)
 
     echo $res > $current_filename
