a8d2ff140bc4cbb768e939d65fc26a30c13593e6
Helena Steck <steckhelena@gmail.com>
2021-12-18 07:46:42 -0300

    One more A50 fix

--- .config/i3/scripts/sel_audio_dev.sh
+++ .config/i3/scripts/sel_audio_dev.sh
@@ -5,8 +5,8 @@ res=$(echo "	Headphone;	Speakers" | rofi -sep ";" -dmenu -p "Select audio 
 if [[ "$res" = *"Headphone"* ]]
 then
 	pactl set-card-profile alsa_card.pci-0000_00_1b.0 output:iec958-stereo+input:analog-stereo
-	pactl set-card-profile alsa_card.usb-Astro_Gaming_Astro_A50-00 pro-audio
-	pactl set-default-source alsa_input.usb-Astro_Gaming_Astro_A50-00.pro-input-1
+	pactl set-card-profile alsa_card.usb-Astro_Gaming_Astro_A50-00 output:stereo-game+output:stereo-chat+input:mono-chat
+	pactl set-default-source alsa_input.usb-Astro_Gaming_Astro_A50-00.mono-chat
 	while read -r line
 	do 
 		if [[ "$line" = *"iec958"* ]] 
