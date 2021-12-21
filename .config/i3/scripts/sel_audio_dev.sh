94f3033e0b6bf5874a0035791ec5cfebce9015c5
Helena Steck <steckhelena@gmail.com>
2021-12-21 09:26:08 -0300

    Remove analog stereo input from pci card profile

--- .config/i3/scripts/sel_audio_dev.sh
+++ .config/i3/scripts/sel_audio_dev.sh
@@ -4,7 +4,7 @@ res=$(echo "	Headphone;	Speakers" | rofi -sep ";" -dmenu -p "Select audio 
 
 if [[ "$res" = *"Headphone"* ]]
 then
-	pactl set-card-profile alsa_card.pci-0000_00_1b.0 output:iec958-stereo+input:analog-stereo
+	pactl set-card-profile alsa_card.pci-0000_00_1b.0 output:iec958-stereo
 	pactl set-card-profile alsa_card.usb-Astro_Gaming_Astro_A50-00 output:stereo-game+output:stereo-chat+input:mono-chat
 	pactl set-default-source alsa_input.usb-Astro_Gaming_Astro_A50-00.mono-chat
 	while read -r line
@@ -21,7 +21,7 @@ fi
 
 if [[ $res = *"Speakers"* ]]
 then
-	pactl set-card-profile alsa_card.pci-0000_00_1b.0 output:analog-stereo+input:analog-stereo
+	pactl set-card-profile alsa_card.pci-0000_00_1b.0 output:analog-stereo
 	while read -r line
 	do 
 		if [[ "$line" = *"alsa_output.pci-0000_00_1b.0.analog-stereo"* ]] 
