06039ce98ba4d6b3e550b8f40bd645c251babfcb
Helena Steck <steckhelena@gmail.com>
2022-05-18 10:33:28 -0300

    Add tsc diagnostics to neovim null-ls

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -245,6 +245,7 @@ return require("packer").startup(function(use)
                     null_ls.builtins.formatting.stylua,
                     null_ls.builtins.diagnostics.eslint_d,
                     null_ls.builtins.formatting.eslint_d,
+                    null_ls.builtins.diagnostics.tsc,
                     null_ls.builtins.formatting.prettierd,
                     null_ls.builtins.code_actions.gitsigns,
                     null_ls.builtins.formatting.black,
