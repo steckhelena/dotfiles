3715d6cf786d9cc476eca8ee8155f93056b1172c
Helena Steck <steckhelena@gmail.com>
2023-09-02 10:50:37 -0300

    Fix new sik name

--- .config/i3/scripts/sel_audio_dev.sh
+++ .config/i3/scripts/sel_audio_dev.sh
@@ -4,7 +4,7 @@ res=$(echo "	Headphone;	Speakers" | rofi -sep ";" -dmenu -p "Select audio 
 
 if [[ "$res" = *"Headphone"* ]]
 then
-	pactl set-card-profile bluez_card.88_C9_E8_68_B2_26 a2dp-sink-ldac
+	pactl set-card-profile bluez_card.88_C9_E8_68_B2_26 a2dp-sink
 	pactl set-card-profile alsa_card.usb-Blue_Microphones_Yeti_X_2119SG002298_888-000313110306-00 output:analog-stereo+input:iec958-stereo
 	pactl set-default-source alsa_input.usb-Blue_Microphones_Yeti_X_2119SG002298_888-000313110306-00.analog-stereo.3
 	while read -r line
